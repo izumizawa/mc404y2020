bc3b5de985785ea4ea6323b6cf4b21ae9b7fff34
izumizawa <mariana.megumi@conpec.com.br>
2020-04-23 03:17:22 -0300

    Projeto 4 - mult

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,63 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+// Compare with zero first
+	@R0
+	D=M
+	@ZERO
+	D;JLE
+	@R1
+	D=M
+	@ZERO
+	D;JLE
+// Compare with one
+	@R0
+	D=M-1
+	@RZERO
+	D;JEQ
+	@R1
+	D=M-1
+	@RUM
+	D;JEQ
+// Save R0 in another register
+	@R0
+	D=M
+	@R4
+	M=D
+(LOOP)
+	@R4
+	D=M
+	@R0 
+	M=D+M // R4 = R0+R4
+	@R1
+	D=M
+	M=D-1 // R1 = R1-1
+	@R1
+	D=M-1 
+	@END
+	D;JLE // R1 = 0 exit loop
+	@LOOP
+	0;JMP // BACK TO THE LOOP 
+(ZERO)
+	@R0
+	D=0
+	M=D
+	@R1
+	M=0
+	@END
+	0;JMP
+(RZERO)
+	@R1
+	D=M
+	@R0
+	M=D
+	@END
+	0;JMP
+(RUM)
+(END)
+	@R0
+	D=M
+	@R2
+	M=D
+	@END
+	0;JMP
