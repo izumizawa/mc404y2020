63e5c90b8fda2dc170664b39609453cf99fdbd89
izumizawa <mariana.megumi@conpec.com.br>
2020-05-05 08:15:33 -0300

    Fill.asm dando errado

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,51 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+// ()   ()   
+// ( @ . @) /* 
+// (  () ) /
+
+(LOOP)
+@16834
+D=A
+@tela
+M=D
+	@KDB // Keyboard
+	D=M
+	@BRANCO
+	D;JEQ // Jump to whitening condition
+	@PRETO
+	0;JMP // Else Jump to blackning condition
+(BRANCO)
+	@tela
+	A=M
+	D=M
+	@24575
+	D=A-D
+	@FORA
+	D;JLE
+	@tela
+	A=M
+	M=0 // "White" pixel
+	@tela
+	M=M+1
+	@BRANCO
+	0;JMP // Back to white loop
+(PRETO)
+	@tela
+	A=M
+	D=M
+	@24575
+	D=A-D
+	@FORA
+	D;JLE
+	@tela
+	A=M
+	M=-1 // "Black" pixel
+	@tela
+	M=M+1
+	@PRETO
+	0;JMP // Back to white loop
+(FORA)
+	@LOOP
+	0;JMP // Return to loop
